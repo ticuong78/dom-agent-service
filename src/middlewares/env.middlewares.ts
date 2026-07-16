79b8ac111211e982f4ea3fd22beb64df2f9c3bd7
Cường Lê <cuongle.teencare@gmail.com>
2026-07-16 16:22:39 +0700

    feat(middleware): added env check before logging keys out

--- src/middlewares/env.middlewares.ts
+++ src/middlewares/env.middlewares.ts
@@ -4,7 +4,8 @@ import type { Request, Response, NextFunction } from "express";
 export function injectEnvs(req: Request, res: Response, next: NextFunction) {
   req.env = env;
 
-  console.log("[middleware-injectEnvs] ", req.env);
+  if (req.env.NODE_ENV === "development")
+    console.log("[middleware-injectEnvs] ", req.env);
 
   next();
 }
