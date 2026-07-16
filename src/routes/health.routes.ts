dc644063a7076beefaf4a4e929fcdd4172cb2976
Cường Lê <cuongle.teencare@gmail.com>
2026-07-16 18:35:45 +0700

    oke

--- src/routes/health.routes.ts
+++ src/routes/health.routes.ts
@@ -1,12 +1,14 @@
-import { countries } from "@/instances/countries.instance.js";
+// import { countries } from "@/instances/countries.instance.js";
+
+// mot ti nua instances se duoc truyen xuong
 import express from "express";
 import type { Request, Response } from "express";
 
 const healthRouter = express.Router();
 
 healthRouter.get("/alive", (req: Request, res: Response) => {
-  if (countries.length !== 0) console.log("[health] Countries still persists.");
-  else console.log("[health] Countries data needs fetch.");
+  // if (countries.length !== 0) console.log("[health] Countries still persists.");
+  // else console.log("[health] Countries data needs fetch.");
   res.status(200).send("Alive");
 });
 
