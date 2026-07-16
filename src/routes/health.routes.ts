fc84a11dd02d774ebcdfe2edab6fb61b69bb26b4
Cường Lê <cuongle.teencare@gmail.com>
2026-07-16 16:18:57 +0700

    feat(health): added logs whenever a call comes

--- src/routes/health.routes.ts
+++ src/routes/health.routes.ts
@@ -5,7 +5,8 @@ import type { Request, Response } from "express";
 const healthRouter = express.Router();
 
 healthRouter.get("/alive", (req: Request, res: Response) => {
-  if (countries.length !== 0) console.log("Countries still persists.");
+  if (countries.length !== 0) console.log("[health] Countries still persists.");
+  else console.log("[health] needs fetch.");
   res.status(200).send("Alive");
 });
 
